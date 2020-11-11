b4784e4099197f9dc4c3c8e2ebfabde637343224
parazyd <parazyd@dyne.org>
2020-11-11 20:58:41 +0100

    Sepearate config into a module. Improve design and layout.

---
+++ config.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# Beerware License, parazyd <parazyd@dyne.org>
+
+from collections import OrderedDict
+
+server = "https://apibay.org"
+
+btc = "1Jexqn23JAiyCCFY3pvncuDkA3EiNn3Bst"
+
+categories = OrderedDict({
+    100: "Audio",
+    101: "Music",
+    102: "Audio Books",
+    103: "Sound Clips",
+    104: "FLAC",
+    199: "Other",
+    200: "Video",
+    201: "Movies",
+    202: "Movies DVDR",
+    203: "Music Videos",
+    204: "Movie Clips",
+    205: "TV-Shows",
+    206: "Handheld",
+    207: "HD Movies",
+    208: "HD TV-Shows",
+    209: "3D",
+    299: "Other",
+    300: "Applications",
+    301: "Windows",
+    302: "Mac/Apple",
+    303: "UNIX",
+    304: "Handheld",
+    305: "IOS(iPad/iPhone)",
+    306: "Android",
+    399: "Other OS",
+    400: "Games",
+    401: "PC",
+    402: "Mac/Apple",
+    403: "PSx",
+    404: "XBOX360",
+    405: "Wii",
+    406: "Handheld",
+    407: "IOS(iPad/iPhone)",
+    408: "Android",
+    499: "Other OS",
+    500: "Porn",
+    501: "Movies",
+    502: "Movies DVDR",
+    503: "Pictures",
+    504: "Games",
+    505: "HD-Movies",
+    506: "Movie Clips",
+    599: "Other",
+    600: "Other",
+    601: "E-books",
+    602: "Comics",
+    603: "Pictures",
+    604: "Covers",
+    605: "Physibles",
+    699: "Other",
+})
